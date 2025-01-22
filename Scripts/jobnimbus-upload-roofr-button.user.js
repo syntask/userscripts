e02ed3d1195b8d95a8abb9d568d7f22d56e0f4e0
syntask <161401651+syntask@users.noreply.github.com>
2025-01-22 11:40:20 -0600

    Update jobnimbus-upload-roofr-button.user.js

--- Scripts/jobnimbus-upload-roofr-button.user.js
+++ Scripts/jobnimbus-upload-roofr-button.user.js
@@ -2,9 +2,11 @@
 // @name        JobNimbus Upload RoofR Button
 // @match       *://webappui.jobnimbus.com/*
 // @grant       GM_xmlhttpRequest
-// @version     1.1
+// @version     1.0.0 build 2501221139
 // @author      Syntask
 // @description Adds a button to upload a RoofR report
+// @downloadURL  https://raw.githubusercontent.com/syntask/userscripts/main/Scripts/jobnimbus-upload-roofr-button.user.js
+// @updateURL    https://raw.githubusercontent.com/syntask/userscripts/main/Scripts/jobnimbus-upload-roofr-button.user.js
 // ==/UserScript==
 
 // Build 202501221129
